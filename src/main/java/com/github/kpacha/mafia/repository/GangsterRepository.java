ffdbfb9825fe14088bbfa87611d6876082faa289
Kpacha <kpacha666@gmail>
2013-01-05 18:03:29 +0100

    Getting some intel about the gang

--- src/main/java/com/github/kpacha/mafia/repository/GangsterRepository.java
+++ src/main/java/com/github/kpacha/mafia/repository/GangsterRepository.java
@@ -15,6 +15,8 @@ public interface GangsterRepository extends GraphRepository<Gangster>,
 	NamedIndexRepository<Gangster>,
 	RelationshipOperationsRepository<Gangster> {
 
+    public int DEFAULT_DEEP = 5;
+
     Page<Gangster> findByNameLike(String name, Pageable page);
 
     @Query("start gangster=node({0}) match gangster<-[r:MANAGES]-boss where boss.onDuty = true return boss")
@@ -25,4 +27,27 @@ public interface GangsterRepository extends GraphRepository<Gangster>,
 
     @Query("start gangster=node({0}) match gangster<-[:MANAGES]-boss-[r:MANAGES]->collegue where boss.onDuty = true and collegue.onDuty = true return collegue order by r.createdAt")
     Set<Gangster> getActiveCollegues(Gangster gangster);
+
+    @Query("start sub=node({0}) match sub<-[r:MANAGES*1.."
+	    + DEFAULT_DEEP
+	    + "]-boss where boss.onDuty=true and NOT (boss<-[:MANAGES]-()) return distinct boss")
+    Set<Gangster> findRoot(Gangster gangster);
+
+    @Query("start sub=node({0}) match sub<-[r:MANAGES*1..{1}]-boss where boss.onDuty=true and NOT (boss<-[:MANAGES]-()) return distinct boss")
+    Set<Gangster> findRoot(Gangster gangster, int deep);
+
+    @Query("start gangster=node({0}) match gangster-[:KNOWS*1.." + DEFAULT_DEEP
+	    + "]-known return count(*)")
+    Set<Integer> countKnown(Gangster gangster);
+
+    @Query("start gangster=node({0}) match gangster-[:KNOWS*1..{1}]-known return count(*)")
+    Set<Integer> countKnown(Gangster gangster, int deep);
+
+    @Query("start sub=node({0}) match p=sub<-[r:MANAGES*0.."
+	    + DEFAULT_DEEP
+	    + "]-boss where boss.onDuty=true and NOT (boss<-[:MANAGES]-()) return MIN(LENGTH(p))")
+    Set<Integer> getLevel(Gangster gangster);
+
+    @Query("start sub=node({0}) match p=sub<-[r:MANAGES*0..{1}]-boss where boss.onDuty=true and NOT (boss<-[:MANAGES]-()) return MIN(LENGTH(p))")
+    Set<Integer> getLevel(Gangster gangster, int deep);
 }
